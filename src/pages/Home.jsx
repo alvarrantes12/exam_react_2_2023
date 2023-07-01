2aabe576a41dc787b67138fb6f5fc0850c120836
KendallC09 <kendallconejo00@gmail.com>
2023-07-01 09:38:42 -0600

    KCA-C02310 second commit done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,53 +1,15 @@
 import React, { useState } from 'react';
 import './Home.css';
-import { postFetch } from '../commons/ApiMethods';
 import Index from '../components/Countries';
+import New from '../components/Countries/new';
 
 function Home() {
-  const [name, setName] = useState('')
-  const [fact, setFact] = useState('')
-  const [message, setMessage] = useState('')
   const [refresh, setRefresh] = useState(true)
-
-  const handleSubmit = async (event) => {
-    event.preventDefault()
-    try {
-      postFetch('countries', { name: name}, {fact: fact })
-        .then((response) => {
-          setName('')
-          setFact('')
-          setMessage('Creado con exito!')
-          setRefresh(true)
-        })
-    } catch (error) {
-      console.log(error)
-    }
-  }
-
 
   return (
     <div className="new-container">
       <Index refresh={refresh} setRefresh={setRefresh}/>
-      <h3>Crear un nuevo país</h3>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <input
-            type="text"
-            value={name}
-            placeholder="Nombre del país"
-            onChange={(e) => { setName(e.target.value) }} />
-        </div>
-        <div>
-          <input
-            type="text"
-            value={fact}
-            placeholder="Dato curioso"
-            onChange={(e) => { setFact(e.target.value) }} />
-        </div>
-
-        <div>{message ? <p>{message}</p> : <br />}</div>
-        <button className="btn" type="submit">Crear país</button>
-      </form>
+      <New setRefresh={setRefresh}/>
     </div>
   );
 
