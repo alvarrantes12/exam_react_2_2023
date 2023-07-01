c1e425492538e97cbfb525c5820e2dff356fa285
status <z0d80a@gmail.com>
2023-07-01 10:49:33 -0600

    DARA-C1624 PARTTWODONE

---
+++ src/components/Countries/new.jsx
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+import { postFetch } from '../../commons/ApiMethods';
+
+function New({ setRefresh }) {
+  const [name, setName] = useState('');
+  const [fact, setFact] = useState('');
+  const [message, setMessage] = useState('');
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      postFetch('countries', { name: name, fact: fact })
+        .then((response) => {
+          setName('');
+          setFact('');
+          setMessage('Creado correctamente');
+          setRefresh(true);
+        });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <div>
+      <h3>Crear un pais</h3>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <input
+            type="text"
+            value={name}
+            placeholder="Nombre del pais"
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
+        <div>
+          <input
+            type="text"
+            value={fact}
+            placeholder="Dato curioso"
+            onChange={(e) => setFact(e.target.value)}
+          />
+        </div>
+        <div>{message ? <p>{message}</p> : <br />}</div>
+        <button>Crear pais</button>
+      </form>
+    </div>
+  );
+}
+
+export default New;
