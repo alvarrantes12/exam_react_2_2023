677ff4a9ea5453efac325fe8ea7dcbde6a136360
EsAllanCastro <allan.castro@ucrso.info>
2023-07-01 10:20:17 -0600

    ACS-C01950 Exam 2 point 7 done React

---
+++ src/commons/List.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+const List = ({ contents }) => {
+
+    if (!contents || contents.length === 0)
+    return <p style={{ display: 'flex', gap: '10px', justifyContent: "center", marginTop: '10px' }}>No hay contenido para mostrar</p>
+
+    return (
+
+        <div style={{ display: 'flex', gap: '10px', marginTop: '10px' }}>
+            <table>
+                <tr>
+                    <th style={{ padding: "1em", backgroundColor: "#090058", fontWeight: "bold", color: "white" }}>ID</th>
+                    <th style={{ padding: "1em", backgroundColor: "#090058", fontWeight: "bold", color: "white"  }}>Nombre del País</th>
+                    <th style={{ padding: "1em", backgroundColor: "#090058", fontWeight: "bold", color: "white"  }}>Dato Curioso</th>
+                </tr>
+
+                <tbody>
+                    {contents.map(({ id, name, fact }) => (
+                        <tr>
+                            <td style={{ padding: "1em", backgroundColor: "#BECCFF", fontWeight: "bold", textAlign: "center"}} >{id}</td>
+                            <td style={{ padding: "1em", backgroundColor: "#BECCFF", textAlign: "center"}} >{name}</td>
+                            <td style={{ padding: "1em", backgroundColor: "#BECCFF", textAlign: "center"}} >{fact}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    );
+}
+
+export default List;
---
+++ src/components/Country/index.jsx
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from 'react';
+import List from '../../commons/List';
+import WithLoadingState from '../../commons/WithLoadingState';
+import { getFetch } from '../../commons/ApiMethods';
+
+function Index({ refresh, setRefresh }) {
+    const LoadingList = WithLoadingState(List)
+    const [contents, setContents] = useState([])
+    const [loading, setLoading] = useState(false)
+
+    useEffect(() => {
+        if (!refresh) return
+        setLoading(true)
+        getFetch('countries').then((data) => {
+            setContents(data)
+            setLoading(false)
+        });
+        setRefresh(false)
+    }, [setContents, setLoading, refresh])
+
+    return (
+        <div>
+            <h1>Examen II </h1>
+            <h2>C01950-C09317</h2>
+            <LoadingList isloading={loading} contents={contents} />
+        </div>
+    )
+};
+
+export default Index;
